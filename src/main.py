c02630265f885c8fc844cf015367784f099d91a8
hgawhari <hgawhari@student.42.fr>
2026-08-12 09:21:02 +0200

    graph: pass data to graph

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import sys
 
 from src.exception import ParserError
 from src.parser.parser import MapParser
+from src.routing.graph import Graph
 
 
 def main() -> int:
@@ -18,10 +19,27 @@ def main() -> int:
         print(f"Error: {error}")
         return 1
 
-    print(f"Drones: {fly_map.nb_drones}")
-    print(f"Hubs: {len(fly_map.hubs)}")
-    print(f"Connections: {len(fly_map.connections)}")
+    # print(f"Drones: {fly_map.nb_drones}")
+    # print(f"Hubs: {len(fly_map.hubs)}")
+    # print(f"Connections: {len(fly_map.connections)}")
 
+    # print("Start:", fly_map.start_hub)
+    # print("Goal:", fly_map.end_hub)
+
+    # path = Path("maps/easy/02_simple_fork.txt")
+
+    # fly_map = MapParser().parse_file(path)
+
+    graph = Graph(fly_map)
+
+    print("Start:", fly_map.start_hub)
+    print("Goal:", fly_map.end_hub)
+    print(fly_map.connections)
+
+    # print("Neighbors of start:", graph.neighbors("start"))
+    # print("Neighbors of junction:", graph.neighbors("waypoint1"))
+    # print("Neighbors of path_a:", graph.neighbors("waypoint2"))
+    # print("Neighbors of goal:", graph.neighbors("goal"))
     return 0
 
 
--- src/models/map.py
+++ src/models/map.py
@@ -7,7 +7,8 @@ from src.models.connection import Connection
 class FlyMap(BaseModel):
     nb_drones: int = Field(gt=0)
 
-    hubs: dict[str, Hub] = {}
-    connections: list[Connection] = []
+    hubs: dict[str, Hub] = Field(default_factory=dict)
+    connections: list[Connection] = Field(default_factory=list)
+
     start_hub: str | None = None
     end_hub: str | None = None
